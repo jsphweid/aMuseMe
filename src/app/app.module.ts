ab975e42b688517eee3f6250c9d5a95f601fe898
Joseph Weidinger <jsphweid@gmail.com>
2016-12-27 13:14:53 -0600

    basic structure without functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+// AngularFire
+import { AngularFireModule } from 'angularfire2';
+
+// bootstrap
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAhK6cZF-p6UW8OI0saGRPxEJ1YvIkv8jI",
+    authDomain: "amuseme-19626.firebaseapp.com",
+    databaseURL: "https://amuseme-19626.firebaseio.com",
+    storageBucket: "amuseme-19626.appspot.com",
+    messagingSenderId: "570604792009"
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     FormsModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+    ]),
     HttpModule
   ],
   providers: [],
